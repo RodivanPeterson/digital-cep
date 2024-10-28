b3c0d45e8a643ea8e1be1612cc793ffe78954e52
Rodivan Peterson <peterodi57@gmail.com>
2024-10-28 17:33:30 -0300

    ajusta a sintaxe do código conforme a PSR 12 através do PHPCodeSniffer

--- src/Search.php
+++ src/Search.php
@@ -6,7 +6,8 @@ class Search
 {
     private const API_URL = "https://viacep.com.br/ws/";
 
-    public function getAddressFromZipCode(string $zipCode): array {
+    public function getAddressFromZipCode(string $zipCode): array
+    {
         $formattedZipCode = $this->formatZipCode($zipCode);
         $response = $this->fetchAddressData($formattedZipCode);
         $addressData = $this->decodeJsonResponse($response);
@@ -14,21 +15,24 @@ class Search
         return $addressData ?: [];
     }
 
-    public function formatZipCode(string $zipCode): string {
+    public function formatZipCode(string $zipCode): string
+    {
         return preg_replace('/[^0-9]/', '', $zipCode);
     }
 
-    public function fetchAddressData(string $zipCode) {
+    public function fetchAddressData(string $zipCode)
+    {
         $response = @file_get_contents(self::API_URL . $zipCode . "/json");
 
         if ($response === false) {
             throw new \Exception("Não foi possível acessar a API de CEP.");
         }
-        
+
         return $response;
     }
 
-    public function decodeJsonResponse(string $response): array {
+    public function decodeJsonResponse(string $response): array
+    {
         $addressData = json_decode($response, true);
 
         if (json_last_error() !== JSON_ERROR_NONE) {
